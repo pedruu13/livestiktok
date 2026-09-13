c911026c716ff037b7ff4d6d622311498b3bf6bc
Dev Pipa Combate <dev@pipacombate.com>
2026-09-13 17:22:33 -0300

    feat: Usa imagens geradas separadamente para personagens e cenario

---
+++ measure.js
@@ -0,0 +1,6 @@
+﻿const { Jimp } = require('jimp');
+async function run() {
+    const img = await Jimp.read('public/games/luta/assets/bolso_sprites.png');
+    console.log(img.bitmap.width, img.bitmap.height);
+}
+run();
